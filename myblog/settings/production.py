712073b75feb0cab74b4a7f69a0243f7577ff13e
Kostja P <kpavlovsky@users.noreply.github.com>
2023-07-29 07:24:23 +0200

    Initial commit

---
+++ myblog/settings/production.py
@@ -0,0 +1,23 @@
+import environ
+import os
+from .base import *
+
+DEBUG = False
+
+try:
+    from .local import *
+except ImportError:
+    pass
+
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+SECRET_KEY = env('SECRET_KEY')
+
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['*'])
+
+DATABASES = env.db()
+
